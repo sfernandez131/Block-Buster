ebc9474c81cc337803d53df61b663be591a326ce
Scott Fernandez <sfernandez131@gmail.com>
2021-11-10 22:28:29 -0500

    Rev1

---
+++ game.py
@@ -0,0 +1,82 @@
+import pygame as pg
+import random as rnd
+import time
+
+
+pg.init()
+clk = pg.time.Clock()
+
+effect = pg.mixer.Sound('sound96.wav')
+winEffect = pg.mixer.Sound('sound100.wav')
+
+mainScrn = pg.display.set_mode((30,40))
+sc = 0
+
+def main(score):
+	playerX = 80
+	playerY = 150
+	speed = 40
+	
+	hasTouched = False
+	rect2 = pg.Rect(rnd.randint(10, 1000), rnd.randint(10, 700), 100, 100)
+	
+	running = True
+	while (running):
+		clk.tick(60)
+		
+		mainScrn.fill(0)
+		
+		scoreDisp(score)
+		
+		rect1 = pg.Rect(playerX, playerY, 100, 100)
+		
+		for event in pg.event.get():
+			if event.type == pg.QUIT:
+				running = False
+	
+			if event.type == pg.KEYDOWN:
+				if event.key == pg.K_w or pg.K_UP:
+					effect.play()
+					playerY -= speed
+				if event.key == pg.K_s or pg.K_DOWN:
+					effect.play()
+					playerY += speed
+				if event.key == pg.K_a or pg.K_LEFT:
+					effect.play()
+					playerX -= speed
+				if event.key == pg.K_d or pg.K_RIGHT:
+					effect.play()
+					playerX += speed
+
+		pg.draw.rect(mainScrn, (255, 0, 0), rect1)
+		pg.draw.rect(mainScrn, (0, 255, 0), rect2)
+		pg.draw.rect(mainScrn, (0, 0, 255), mainScrn.get_rect(), 3)
+		
+		if rect1.colliderect(rect2):
+			hasTouched = True
+		
+		if hasTouched:
+			winEffect.play()
+			score += 1
+			mainScrn.fill(0)
+			gameOverFont = pg.font.Font('freesansbold.ttf', 72)
+			gameOverSurf = gameOverFont.render('WIN', True, (255, 255, 200))
+			gameOverRect = gameOverSurf.get_rect()
+			gameOverRect.midtop = mainScrn.get_rect().center
+			mainScrn.blit(gameOverSurf, gameOverRect)
+			pg.display.flip()
+			time.sleep(2)
+			main(score)
+	
+		pg.display.flip()
+	pg.quit()
+
+
+def scoreDisp(score):
+	scoreFont = pg.font.Font('freesansbold.ttf', 72)
+	gameOverSurf = scoreFont.render("SCORE: " + str(score), True, (255, 255, 255))
+	gameOverRect = gameOverSurf.get_rect()
+	gameOverRect.topleft = (40, 30)
+	mainScrn.blit(gameOverSurf, gameOverRect)
+		
+main(sc)
